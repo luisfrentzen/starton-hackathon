5f64650559426e12e90e33d6446469cfdfeb260c
lukast2410 <lt24k24@gmail.com>
2021-09-07 05:38:22 +0700

    Add infinite questions

--- react-frontend/src/components/Navbar.js
+++ react-frontend/src/components/Navbar.js
@@ -38,7 +38,6 @@ export default function Navbar() {
                       src="/assets/scholarr_left.png"
                       alt="CHOLARR"
                     />
-                    {/* <span className='hidden sm:block font-bold text-lg ml-3'>CHolaRR</span> */}
                   </Link>
                 </div>
               </div>
@@ -71,73 +70,12 @@ export default function Navbar() {
           <Disclosure.Panel className="md:hidden">
             <div className="pt-2 pb-3 space-y-1">
               {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
-              <a
-                href="#"
-                className="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
+              <Link
+                to='/majors'
+                className={`${location.pathname.includes('majors') ? 'bg-blue-100 border-secondary-400 text-secondary-200' : 'border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700'}  block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6`}
               >
-                Dashboard
-              </a>
-              <a
-                href="#"
-                className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
-              >
-                Team
-              </a>
-              <a
-                href="#"
-                className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
-              >
-                Projects
-              </a>
-              <a
-                href="#"
-                className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
-              >
-                Calendar
-              </a>
-            </div>
-            <div className="pt-4 pb-3 border-t border-gray-200">
-              <div className="flex items-center px-4 sm:px-6">
-                <div className="flex-shrink-0">
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                </div>
-                <div className="ml-3">
-                  <div className="text-base font-medium text-gray-800">
-                    Tom Cook
-                  </div>
-                  <div className="text-sm font-medium text-gray-500">
-                    tom@example.com
-                  </div>
-                </div>
-                <button className="ml-auto flex-shrink-0 bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-              </div>
-              <div className="mt-3 space-y-1">
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6"
-                >
-                  Your Profile
-                </a>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6"
-                >
-                  Settings
-                </a>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6"
-                >
-                  Sign out
-                </a>
-              </div>
+                Majors
+              </Link>
             </div>
           </Disclosure.Panel>
         </>
--- react-frontend/src/pages/TestPage.js
+++ react-frontend/src/pages/TestPage.js
@@ -2,6 +2,7 @@ import { ChevronRightIcon } from '@heroicons/react/outline'
 import { useState } from 'react'
 import '../styles/Home.css'
 import ResultModal from '../components/ResultModal'
+import Question from '../components/Question'
 
 export default function TestPage() {
 	const maxLength = 450
@@ -10,13 +11,13 @@ export default function TestPage() {
 		setWord(e.target.value)
 	}
 
-	const submitHandler = () => {
+	const submitHandler = (word) => {
 		// setLoading(true)
-		fetch("/predict", {
+		fetch('/predict', {
 			headers: {
-				"Content-Type": "application/json",
+				'Content-Type': 'application/json',
 			},
-			method: "POST",
+			method: 'POST',
 			body: JSON.stringify({
 				essay: word,
 			}),
@@ -31,8 +32,20 @@ export default function TestPage() {
 				// setMajors(res.majors)
 				// setmodalOpened(true)
 			})
+
+		//next slide
+		setQuestion((q) => [...q, <Question title='Testing' desc='description' example='example' maxLength={maxLength} submitHandler={submitHandler}/>])
+
+		let activeSlide = document.querySelector('.slide.translate-x-0')
+		activeSlide.classList.remove('translate-x-0')
+		activeSlide.classList.add('-translate-x-full')
+
+		let nextSlide = activeSlide.nextElementSibling
+		nextSlide.classList.remove('translate-x-full')
+		nextSlide.classList.add('translate-x-0')
 	}
 
+	const [question, setQuestion] = useState([<Question title='Testing' desc='description' example='example' maxLength={maxLength} submitHandler={submitHandler}/>])
 	const [isExample, setExample] = useState(false)
 	const [word, setWord] = useState('')
 	const [isLoading, setLoading] = useState(false)
@@ -42,7 +55,7 @@ export default function TestPage() {
 
 	return (
 		<div
-			className='bg-cover bg-left-bottom relative overflow-hidden'
+			className='bg-cover w-screen h-screen bg-left-bottom relative overflow-hidden'
 			style={{ backgroundImage: 'url(/assets/pattern-blue.jpg)' }}
 		>
 			<div
@@ -65,55 +78,58 @@ export default function TestPage() {
 					probabilities={probabilities}
 				></ResultModal>
 			</div>
-			<div className='w-screen h-screen transition-all ease-in-out duration-1000 transform translate-x-0 slide'>
-				<div className='mx-auto h-screen max-w-screen-md flex items-center'>
-					<div className='py-8 w-full text-center pb-20 mx-10'>
-						<div className='flex sm:justify-between w-full sm:items-center mb-4 flex-col sm:flex-row'>
-							<div className='text-left'>
-								<p className='text-xl sm:text-3xl font-bold text-white'>Tell me about yourself</p>
-								<p className='text-xs sm:text-base text-gray-200 mt-1'>
-									A bit lost ? Feel free to check on my example behind this card.
-								</p>
-							</div>
-							<button
-								type='button'
-								className='inline-flex items-center justify-center px-4 py-2 border border-gray-300 shadow-sm text-xs sm:text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-100 focus:outline-none w-24 sm:w-28 sm:m-0 mt-2'
-								onClick={() => setExample(!isExample)}
-							>
-								{isExample ? 'Write' : 'Example'}
-							</button>
-						</div>
-
-						<div className='flip h-80'>
-							<div className={`flip-content w-full ${isExample ? 'flip-content-clicked' : ''}`}>
-								<div className='flip-input w-full'>
-									<textarea
-										spellCheck='false'
-										className='w-full rounded-lg shadow-md outline-none resize-none border border-gray-200 h-80 text-sm sm:text-lg py-4 px-6 placeholder-gray-300 placeholder-opacity-100'
-										placeholder='I love business and making money'
-										maxLength={maxLength}
-										onChange={inputListener}
-									></textarea>
-									<div className='sm:text-base text-xs text-gray-300 -mt-8 mr-4 sm:-mt-10 sm:mr-4 text-right'>
-										{word.length} / {maxLength}
-									</div>
+			<div className='relative flex'>
+				<div className='absolute inset-0 w-screen h-screen transition-all ease-in-out duration-1000 transform translate-x-0 slide'>
+					<div className='mx-auto h-screen max-w-screen-md flex items-center'>
+						<div className='py-8 w-full text-center pb-20 mx-10'>
+							<div className='flex sm:justify-between w-full sm:items-center mb-4 flex-col sm:flex-row'>
+								<div className='text-left'>
+									<p className='text-xl sm:text-3xl font-bold text-white'>Tell me about yourself</p>
+									<p className='text-xs sm:text-base text-gray-200 mt-1'>
+										A bit lost ? Feel free to check on my example behind this card.
+									</p>
 								</div>
-								<div className='flip-example bg-indigo-900 text-gray-300 p-4 rounded-lg w-full h-80 text-left py-4 px-6 text-lg'>
-									I love to learn about computer science and problem solving. I think I'm really good at
-									mathematics and critical thinking.
+								<button
+									type='button'
+									className='inline-flex items-center justify-center px-4 py-2 border border-gray-300 shadow-sm text-xs sm:text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-100 focus:outline-none w-24 sm:w-28 sm:m-0 mt-2'
+									onClick={() => setExample(!isExample)}
+								>
+									{isExample ? 'Write' : 'Example'}
+								</button>
+							</div>
+
+							<div className='flip h-80'>
+								<div className={`flip-content w-full ${isExample ? 'flip-content-clicked' : ''}`}>
+									<div className='flip-input w-full'>
+										<textarea
+											spellCheck='false'
+											className='w-full rounded-lg shadow-md outline-none resize-none border border-gray-200 h-80 text-sm sm:text-lg py-4 px-6 placeholder-gray-300 placeholder-opacity-100'
+											placeholder='I love business and making money'
+											maxLength={maxLength}
+											onChange={inputListener}
+										></textarea>
+										<div className='sm:text-base text-xs text-gray-300 -mt-8 mr-4 sm:-mt-10 sm:mr-4 text-right'>
+											{word.length} / {maxLength}
+										</div>
+									</div>
+									<div className='flip-example bg-indigo-900 text-gray-300 p-4 rounded-lg w-full h-80 text-left py-4 px-6 text-lg'>
+										I love to learn about computer science and problem solving. I think I'm really good at
+										mathematics and critical thinking.
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
+					<div className='absolute bottom-10 left-1/2 lg:bottom-auto lg:left-auto lg:right-16 lg:top-1/2 flex hover:animate-bounce-right '>
+						<button
+							className='rounded-full shadow-md bg-green-400 p-3 hover:bg-green-500 -ml-6'
+							onClick={() => submitHandler(word)}
+						>
+							<ChevronRightIcon className='w-6 h-6 sm:h-10 sm:w-10 text-gray-800' />
+						</button>
+					</div>
 				</div>
-				<div className='absolute bottom-10 left-1/2 lg:bottom-auto lg:left-auto lg:right-16 lg:top-1/2 flex hover:animate-bounce-right '>
-					<button
-						className='rounded-full shadow-md bg-green-400 p-3 hover:bg-green-500 -ml-6'
-						onClick={submitHandler}
-					>
-						<ChevronRightIcon className='w-6 h-6 sm:h-10 sm:w-10 text-gray-800' />
-					</button>
-				</div>
+				{question}
 			</div>
 		</div>
 	)
